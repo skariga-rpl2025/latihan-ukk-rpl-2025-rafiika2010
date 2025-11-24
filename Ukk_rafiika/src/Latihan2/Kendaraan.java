6d33030cea14f3a912c41d8cb3a475f0927755b2
rafiika2010 <rafiikaditya@gmail.com>
2025-11-24 14:59:23 +0700

    Kendaraan

---
+++ Ukk_rafiika/src/Latihan2/Kendaraan.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan2;
+
+/**
+ *
+ * @author Lab Studio
+ */
+public class Kendaraan {
+    private String Nopol ;
+    private String NamaMerk ;
+    private int TahunTerbit;
+    
+    
+    
+    public String setNopol(){
+        return Nopol;
+    }
+    public void setNopol(String Nopol){
+        this.Nopol = Nopol ;
+    }
+    public String setNamaMerk(){
+        return NamaMerk;
+    }
+    public void setNamaMerk(String NamaMerk){
+        this.NamaMerk = NamaMerk;
+    }
+    public int setTahunTerbit(){
+        return TahunTerbit;
+    }
+    public void setTahunTerbit(int TahunTerbit){
+        this.TahunTerbit = TahunTerbit;
+    }
+}
+
